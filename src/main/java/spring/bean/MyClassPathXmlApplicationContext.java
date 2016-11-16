c2552d00bda52032cce1c06726224d2d2211afb7
shiqining <shiqining@lianjia.com>
2016-11-16 10:22:28 +0800

    add MyClassPathXmlApplicationContext

---
+++ src/main/java/spring/bean/MyClassPathXmlApplicationContext.java
@@ -0,0 +1,23 @@
+package spring.bean;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Created by shiqining on 11/15/16.
+ */
+public class MyClassPathXmlApplicationContext extends ClassPathXmlApplicationContext {
+
+    public MyClassPathXmlApplicationContext(String... configLocations) throws BeansException {
+        super(configLocations);
+    }
+
+    protected void initPropertySources() {
+        getEnvironment().setRequiredProperties("VAR");
+    }
+
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new MyClassPathXmlApplicationContext("spring/spring-beans.xml");
+    }
+}
