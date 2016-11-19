b4a24f3f76b4036be90d704bdc76fb68c593fb70
shiqining <shiqining@lianjia.com>
2016-11-19 16:27:45 +0800

    实现BeanFactoryPostProcessor

--- src/main/java/spring/bean/MyClassPathXmlApplicationContext.java
+++ src/main/java/spring/bean/MyClassPathXmlApplicationContext.java
@@ -3,6 +3,7 @@ package spring.bean;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.env.Environment;
 
 /**
  * Created by shiqining on 11/15/16.
@@ -19,5 +20,8 @@ public class MyClassPathXmlApplicationContext extends ClassPathXmlApplicationCon
 
     public static void main(String[] args) {
         ApplicationContext applicationContext = new MyClassPathXmlApplicationContext("spring/spring-beans.xml");
+        UserManager userManager = (UserManager) applicationContext.getBean("userManager");
+        System.out.println(userManager);
+        Environment environment = userManager.getEnvironment();
     }
 }
