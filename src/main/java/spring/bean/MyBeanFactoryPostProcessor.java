2208683b0f1079b9d899316a348157372717bcae
shiqining <shiqining@lianjia.com>
2016-11-19 16:27:34 +0800

    实现BeanFactoryPostProcessor

---
+++ src/main/java/spring/bean/MyBeanFactoryPostProcessor.java
@@ -0,0 +1,15 @@
+package spring.bean;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+
+/**
+ * Created by shiqining on 11/17/16.
+ */
+public class MyBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+        System.out.println("postProcessBeanFactory");
+    }
+}
