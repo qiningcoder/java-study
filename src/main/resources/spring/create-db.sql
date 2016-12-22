769e15eea2d52a1661977d4294a5528854379ad3
shiqining <shiqining@lianjia.com>
2016-12-22 18:46:18 +0800

    h2

--- src/main/resources/spring/create-db.sql
+++ src/main/resources/spring/create-db.sql
@@ -4,3 +4,16 @@ CREATE TABLE `user` (
   `age` int(11) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+insert into t(a, b) values(1,1), (2,2);
+select * from t;
+
+replace into t (id, a, b) values(1, 2, 2);
+replace into t (a, b) values(4, 4);
+replace into t (a, b) values(4, 5);
+
+
+insert into t(a, b) values(10,10), (20,20);
+
+insert into t (a, b) VALUES (11, 11) on duplicate key update b = 11;
+insert into t (a, b) VALUES (11, 12) on duplicate key update b = 12;
